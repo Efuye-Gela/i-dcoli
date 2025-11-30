2f3302db5e9327f3115117b53167c37d16f53549
Semir_S <149691609+Semirss@users.noreply.github.com>
2025-11-30 19:09:26 +0300

    finished some ui stuff and updated the code related to gamemanager

---
+++ Idcoli/Assets/Scenes/pausescript.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class pausescript : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject image;
+    [SerializeField] private GameObject joystic;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseCanvas.SetActive(false); // hide at start
+        image.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Press ESC or P to toggle pause
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        image.SetActive(true);
+        menu.SetActive(false);
+        joystic.SetActive(!isPaused);
+
+        pauseCanvas.SetActive(isPaused);
+
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        image.SetActive(false);
+        pauseCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        joystic.SetActive(true);
+        menu.SetActive(false);
+    }
+}
